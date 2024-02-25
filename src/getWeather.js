3931e60881f88fdde5a2b63de0dfc70d1d500950
Yu Zhou Zhang <yuzspanish@gmail.com>
2024-02-25 00:43:51 +0000

    fix api url

--- src/getWeather.js
+++ src/getWeather.js
@@ -1,14 +1,12 @@
 export default async function getWeather(location) {
   location = location.toLowerCase();
   const api_key = '9163a5b7d3c7495ca4a232649231812';
+  const url = `https://api.weatherapi.com/v1/forecast.json?key=${api_key}&q=${location}`;
 
   try {
-    const data = await fetch(
-      `https://api.weatherapi.com/v1/forecast.json?key=${api_key}&q=${location}`,
-      {
-        mode: 'cors',
-      }
-    );
+    const data = await fetch(url, {
+      mode: 'cors',
+    });
     const weatherData = await data.json();
     return weatherData;
   } catch (error) {
